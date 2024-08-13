f8218c3a284ebd816d9ccdb95a1119ed5f8598aa
Cesiahuezo2005 <19773360@clases.edu.sv>
2024-08-12 19:22:46 -0600

    Delete index directory

--- index/Ejemplo2.js
+++
@@ -1,9 +0,0 @@
-let valor1;
-let valor2;
-valor1=prompt('Ingrese primero número:');
-valor2=prompt('Ingrese segundo número:');
-
-var suma=parseInt(valor1) + parseInt(valor2);
-var producto= parseInt(valor1) * parseInt(valor2);
-document.write ("La suma es:" +"<br>"+ suma);
-alert("El producto es:"+producto);
